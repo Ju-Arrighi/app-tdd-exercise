3b898bafe09c3f1fbdad0ddf79427d0e19fc5897
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-27 17:27:41 +0200

    Project app.

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  scenario 'show welcome massage at root page' do
+    visit('/')
+    expect(page).to have_content('Welcome')
+  end
+  scenario 'verify link client add' do
+    visit(root_path)
+    # expect(page).to have_selector('ul li')
+    # expect(page).to have_link('Add a client')
+    expect(page.find('ul li')).to have_link('Add a client')
+  end
 end
